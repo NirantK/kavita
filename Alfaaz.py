ee44b394e4e05095b1f63254f933df894c3023dd
Nirant <NirantK@users.noreply.github.com>
2020-11-12 15:18:08 +0530

    Bring master and dev to same state (#5)
    * Merge to master (#3)
    
    * trying out workflows + streamlit
    
    * Update secrets.yaml
    
    * change message
    
    * revising
    
    * mend
    
    * mend
    
    * mend
    
    * mend
    
    * Update secrets.yaml
    
    * Update twitterbot.py
    
    * adding streamlit
    
    * adding streamlit
    
    * adding streamlit
    
    * Just trying out something
    
    * fixed workflow
    
    * utils
    
    * update
    
    * requirements
    
    * utils
    
    * Fix formatting with black
    
    * Reorganise imports
    
    * Simplify code to one file
    
    * Formatting fixes
    
    Co-authored-by: radhikasethi2011 <radhika.sethi0312@gmail.com>
    Co-authored-by: meghanabhange <meghanabhange13@gmail.com>
    
    * Fix error
    
    * to setup heroku
    
    * mend
    
    * mend
    
    * mend
    
    * Create python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Update python-app.yml
    
    * Added auth check
    
    Co-authored-by: radhikasethi2011 <radhika.sethi0312@gmail.com>
    Co-authored-by: meghanabhange <meghanabhange13@gmail.com>

--- Alfaaz.py
+++ Alfaaz.py
@@ -20,8 +20,17 @@ def get_tweet(tweet_url: str, twitter_API):
         tweet_id = re.findall("(?<=status\/)(.*)(?=\?s)", tweet_url)
         tweet = twitter_API.get_status(tweet_id[0]).text
     except:
-        st.e("Invalid tweet, please try again")
+        st.error("Invalid tweet, please try again")
+        tweet = "Invalid tweet, please try again"
     return tweet
+
+def verify_credentials(twitter_API):
+    try:
+        twitter_API.verify_credentials()
+        print("Authentication Ok")
+    except:
+        print("Error during authentication")
+
 
 
 # credentials = yaml.load(open('TestbaseRadhika/.github/workflows/secrets.yaml'))
@@ -30,9 +39,11 @@ auth.set_access_token(os.environ["ACCESS_KEY"], os.environ["ACCESS_SECRET"])
 st.title("HinglishBot: Sentiment Analysis Tool")
 
 dummy_tweet = "enter a tweet url here, eg - https://twitter.com/Twitter/status/1320822556614676480"
+
 preview_count = 9
 warning_count = 10000
 api = get_twitter_api(auth)
+verify_credentials(api)
 list_of_display_messages = [
     "Asking the crowds what they think",
     "The committee is making up it's mind",
